6dcf4ef82bd8f40a30897a22990057b530e4a1e4
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2018-04-11 16:43:59 -0300

    ongoing work

--- src/AnaliseLexica.java
+++ src/AnaliseLexica.java
@@ -1,12 +1,12 @@
 import java.io.*;
 
-enum TokenType{ NUM,SOMA, MULT,APar,FPar, EOF}
+enum TokenType{ NUM, SOMA, SUB, MULT, DIV, APar, FPar, EOF}
 
 class Token{
-  char lexema;
+  String lexema;
   TokenType token;
 
- Token (char l, TokenType t)
+ Token (String l, TokenType t)
  	{ lexema=l;token = t;}	
 
 }
@@ -34,25 +34,32 @@ class AnaliseLexica {
 				currchar = (char) currchar1;
 			} while (currchar == '\n' || currchar == ' ' || currchar =='\t' || currchar == '\r');
 			
-			if(currchar1 != eof && currchar1 !=10)
-			{
-								
-	
-				if (currchar >= '0' && currchar <= '9')
-					return (new Token (currchar, TokenType.NUM));
-				else
+			if(currchar1 != eof && currchar1 !=10) {
+				if (currchar >= '0' && currchar <= '9') {
+					String numero;
+					do {
+						numero += currchar;
+						currchar1 =  arquivo.read();
+						currchar = (char) currchar1;
+					} while (currchar >= '0' && currchar <= '9');
+					return (new Token (numero, TokenType.NUM));
+				} else {
 					switch (currchar){
 						case '(':
-							return (new Token (currchar,TokenType.APar));
+							return (new Token (new String(currchar),TokenType.APar));
 						case ')':
-							return (new Token (currchar,TokenType.FPar));
+							return (new Token (new String(currchar),TokenType.FPar));
 						case '+':
-							return (new Token (currchar,TokenType.SOMA));
+							return (new Token (new String(currchar),TokenType.SOMA));
+						case '-':
+							return (new Token (new String(currchar),TokenType.SUB));
+						case '/':
+							return (new Token (new String(currchar),TokenType.DIV));
 						case '*':
-							return (new Token (currchar,TokenType.MULT));
-						
+							return (new Token (new String(currchar),TokenType.MULT));
 						default: throw (new Exception("Caractere inválido: " + ((int) currchar)));
 					}
+				}
 			}
 
 			arquivo.close();
